162cab58d72e73ac36ae6d87cdeee3e2c28cdca0
Terje Kvannli <terje@kvannli.com>
2025-02-22 11:41:32 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # blazor-renderfragment-issue
-RenderFragment not able to switch content in Interactive Mode
+
+> [!NOTE]
+> RenderFragment not able to switch content in Interactive Mode. 
+> Using any of the `@rendermode Interactive*` makes the rendering "stop". 
+> Removing the `@rendermode`-line, everything is rendered as expected, but no interaction (button clicks etc) is possible.
+
+![Example](example.gif)
+
+## Example
+
+* `RenderFragmentPageOneAndTwo.razor` combines two routes. Only the first route hit is rendered as expected.
+
+```csharp
+@page "/renderfragment/one"
+@page "/renderfragment/two"
+```
+
+Would expect to show contents of the given `RenderFragment` based on the route.
+
+* `RenderFragmentPageThree.razor` shows only one renderfragment, and it seems to be render correctly.
+* `RenderFragmentPageFour.razor` shows only one renderfragment, and it seems to be render correctly.
---
+++ Source/RenderFragmentIssue/RenderFragmentIssue/Program.cs
@@ -0,0 +1,42 @@
+using RenderFragmentIssue.Client.Pages;
+using RenderFragmentIssue.Components;
+
+namespace RenderFragmentIssue;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.AddServiceDefaults();
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+
+        var app = builder.Build();
+
+        app.MapDefaultEndpoints();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseWebAssemblyDebugging();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+        }
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);
+
+        app.Run();
+    }
+}
